391c7b6c16330fb6724ae4d303f9a3f10a7ed239
Documenter.jl <documenter@juliadocs.github.io>
2023-11-13 09:21:42 +0000

    build based on c75d6da

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DryTooling.jl","page":"Home","title":"DryTooling.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"DryToolingCore.coremean","category":"page"},{"location":"#DryToolingCore.coremean","page":"Home","title":"DryToolingCore.coremean","text":"Compute a mean of x with another function name.\n\n\n\n\n\n","category":"function"}]
+}
