7a5b10cfa3f3bda21891e572041877610d2dcb5f
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2023-11-15 23:31:38 +0100

    Cleaning

--- docs/make.jl
+++ docs/make.jl
@@ -4,11 +4,6 @@ using Documenter.DocMeta: setdocmeta!
 using DocumenterCitations
 using YAML
 
-# For local generation add these to Project.toml:
-# DryToolingCore = "8d904351-f17f-419c-aa8d-05d4f5cffd52"
-# DryToolingGranular = "c4cd2e39-2c11-49af-bb08-d9d53d680cb6"
-# DryToolingKinetics = "6073c425-3840-42bf-8fb8-df8bffbffcc3"
-
 using DryToolingCore
 using DryToolingGranular
 using DryToolingKinetics
@@ -25,29 +20,23 @@ modules = [
 
 function parsepages(x)
     name, target = x["name"], x["target"]
-
     if isa(target, Vector{Dict{Any, Any}})
         return name => map(parsepages, target)
     end
-
     return name => target
 end
 
+authors(c)   = "$(c["name"]) <$(c["mail"])> and contributors"
+repolink(c)  = "https://github.com/$(c["user"])/$(c["site"])"
+canonical(c) = "https://$(c["user"]).github.io/$(c["site"])"
 
-bib = CitationBibliography(joinpath(@__DIR__, "src/references.bib"))
 conf = YAML.load_file(joinpath(@__DIR__, "make.yaml"))
-
-name = conf["name"]
-mail = conf["mail"]
-user = conf["user"]
-site = conf["site"]
-repo = "https://github.com/$(user)/$(site)"
 
 formats = [
     Documenter.HTML(;
         prettyurls = get(ENV, "CI", "false") == "true",
-        canonical  = "https://$(user).github.io/$(site)",
-        repolink   = repo,
+        canonical  = canonical(conf),
+        repolink   = repolink(conf),
         edit_link  = "main",
         assets     = String[],
     ),
@@ -62,10 +51,12 @@ makedocs(;
     format   = formats[1],
     clean    = true,
     sitename = site,
-    authors  = "$(name) <$(mail)>",
-    repo     = "$(repo)/blob/{commit}{path}#{line}",
+    authors  = authors(conf),
+    repo     = "$(repolink(conf))/blob/{commit}{path}#{line}",
     pages    = map(parsepages, conf["pages"]),
-    plugins  = [bib],
+    plugins  = [
+        CitationBibliography(joinpath(@__DIR__, "src/references.bib"))
+    ],
 )
 
 deploydocs(;
